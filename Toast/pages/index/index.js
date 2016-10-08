fd9edb9e7f3409ed7a6b11c6ed71afb8192872b5
Jack_lin <401788217@qq.com>
2016-10-09 05:17:52 +0800

    微信小程序——Toast使用

---
+++ Toast/pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    toast1Hidden :true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onClickBtn:function(){
+    this.setData({
+      toast1Hidden:false
+    })
+
+
+  },
+  toast1Change:function(){
+    this.setData({
+      toast1Hidden:true
+    })
+
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
